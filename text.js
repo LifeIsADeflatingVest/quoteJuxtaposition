121ec13d8303b7bedacaf7ed3b0ced9b0a9469b9
Chris <41416621+LifeIsADeflatingVest@users.noreply.github.com>
2020-12-04 08:34:50 +0200

    Add files via upload

--- text.js
+++ text.js
@@ -14,8 +14,11 @@ function boot() {
 	var keyword = findNouns(quoteText);
 	if (inco) {
 		var keywordArray = 	dataMUSE('https://api.datamuse.com/words?rel_ant='+keyword);
-		if (keywordArray.length!=0) {
+		if (keywordArray.length > 1) {
 			keyword = RiTa.randomItem(keywordArray);
+		}
+		else {
+			keyword = RiTa.randomItem(negativityArray);
 		}
 	}
 	var r = randomColor();
@@ -31,21 +34,26 @@ function boot() {
 			format: "json"
 		},
 		function(data) {
-			var rnd = Math.floor(Math.random() * data.items.length);
-			var desc = (data.items[rnd]['description']);
-			var dims = desc.substring(desc.indexOf("width="));
-			var nums = /\d+/g;
-			var numsTot = (dims.match(nums));
-			var width=numsTot[0];
-			var height=numsTot[1];
-			if (landscapeCheck(width, height)) {
-				var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");	
-				document.getElementById("theImage").setAttribute("src", image_src);
-				document.getElementById("theText").style.fontFamily = (theFont);
-				document.getElementById("theText").innerHTML = (quoteText + "<br> — " + quoteAuthor);
+			if (!jQuery.isEmptyObject({data})) {
+				var rnd = Math.floor(Math.random() * data.items.length);
+				var desc = (data.items[rnd]['description']);
+				var dims = desc.substring(desc.indexOf("width="));
+				var nums = /\d+/g;
+				var numsTot = (dims.match(nums));
+				var width=numsTot[0];
+				var height=numsTot[1];
+				if (landscapeCheck(width, height)) {
+					var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");	
+					document.getElementById("theImage").setAttribute("src", image_src);
+					document.getElementById("theText").style.fontFamily = (theFont);
+					document.getElementById("theText").innerHTML = (quoteText + "<br> — " + quoteAuthor);
+				}
+				else {
+					getImages();
+				}
 			}
 			else {
-				getImages();
+				alert("There was a connection error. Please try again.");
 			}
 		});
 		setTimeout(
@@ -64,18 +72,23 @@ function boot() {
 	}
 	
 	function findNouns(qt) {
-		var s = qt.split(" ");
-		var array = [];
-		for (i=0;i<s.length;i++) {
-			if (RiTa.isNoun(s[i]) && !RiTa.isW_Question(s[i]) && !RiTa.isAdjective(s[i]) &&!RiTa.isAdverb(s[i])) {
-				array.push(s[i]);
-			}
-		}
-		if (array.length != 0) {
-			return RiTa.randomItem(array);
+		if (typeof qt !== 'string') {
+			return "cat";
 		}
 		else {
-			return "cat";
+			var s = qt.split(" ");
+			var array = [];
+			for (i=0;i<s.length;i++) {
+				if (RiTa.isNoun(s[i]) && !RiTa.isW_Question(s[i]) && !RiTa.isAdjective(s[i]) &&!RiTa.isAdverb(s[i])) {
+					array.push(s[i]);
+				}
+			}
+			if (array.length != 0) {
+				return RiTa.randomItem(array);
+			}
+			else {
+				return "cat";
+			}
 		}
 	}
 	
